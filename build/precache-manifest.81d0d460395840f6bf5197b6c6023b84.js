fb9dfec40b5218cc1e0d4f2758820b75d01e3980
lizafalko <lizafloyd2@gmail.com>
2020-07-04 19:31:23 +0300

    added build

---
+++ build/precache-manifest.81d0d460395840f6bf5197b6c6023b84.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a341cad9307b3b132298006c74c71eb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2995ab7246c6c87c89b",
+    "url": "/static/css/main.8a25892b.chunk.css"
+  },
+  {
+    "revision": "369f1aa36368e4705be7",
+    "url": "/static/js/2.1cf23646.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1cf23646.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2995ab7246c6c87c89b",
+    "url": "/static/js/main.e091ef20.chunk.js"
+  },
+  {
+    "revision": "38f52158b64e2d1b03ec",
+    "url": "/static/js/runtime-main.c16ea0ca.js"
+  },
+  {
+    "revision": "1ba9828dd688a4d26c1a3c2a06e28f13",
+    "url": "/static/media/icon01.1ba9828d.svg"
+  },
+  {
+    "revision": "d4a098c086346bc0c3c355f51cdad817",
+    "url": "/static/media/icon02.d4a098c0.svg"
+  },
+  {
+    "revision": "d578b2f424fb2f08d84cf7d5a14f8b7b",
+    "url": "/static/media/icon03.d578b2f4.svg"
+  },
+  {
+    "revision": "eecf9a5ddb9e7e8c7771d915ba1d43b4",
+    "url": "/static/media/icon04.eecf9a5d.svg"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import logo from './logo.svg';
+import Advantages from './Advantages';
+
+function App() {
+  return (
+    <div className="app">
+      <Advantages />
+    </div>
+  );
+}
+
+export default App;
